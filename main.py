63c5f14e1b92d9b761cb2b9eb9d7758a7528ce68
Chamchisengsun <junhoo88888@gmail.com>
2025-03-11 13:05:44 +0900

    feat: Add add function
    #1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def add(x : Union[int, float], y:Union[int,float]) -> Union[int,float]:
     return x + y
 
 if __name__ == "__main__":
-    print(add(1,3))
+    print(add(1,2))
     print(add(1.5, 2.5))
     print(add(1,2.5))
     print(add(1.5,7))
